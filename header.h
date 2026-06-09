36e8f7e60462aaa86f3af4847dced67aad2cd57a
Sadewoahza <hasiqghanisadewo@gmail.com>
2026-06-09 16:54:28 +0700

    masukin header awal

--- header.h
+++ header.h
@@ -1,3 +1,26 @@
-// ini dewo
-//ini coba lagi
-//coba lagi
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+// --- KONSTANTA GLOBAL ---
+#define MAX_HADIAH 100
+#define MAX_GERAK 100
+
+// --- STRUKTUR ---
+typedef struct {
+    int x;
+    int y;
+    char nama[50];
+    int skor;
+} Hadiah;
+
+typedef struct {
+    int x;
+    int y;
+} Gerak;
+
+
+#endif
