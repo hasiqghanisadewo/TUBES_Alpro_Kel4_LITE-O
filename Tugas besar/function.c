76fc1c48d657d429faa167b42cda446358c7e7fc
christinnabata <christinnabata@gmail.com>
2026-06-16 15:50:20 +0700

    membuat fungsi inputGerak

--- Tugas besar/function.c
+++ Tugas besar/function.c
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 #include "header.h"
 
-FILE *f
+FILE *f;
 
 void start(char* nama_file) {
     f = fopen(nama_file, "r");
@@ -17,7 +17,7 @@ void maju(char* kata_output) {
     }
 }
 
-#define FILE_GERAK "tgerak.txt"    // tgerak.txt sebagai NAMA_FILE_GERAK
+#define FILE_GERAK "tgerak.txt"    // tgerak.txt sebagai FILE_GERAK
 
 // membaca semua data gerak dari file tgerak.txt
 void bacaGerak(Gerak arr[], int *n) {
@@ -46,22 +46,24 @@ void bacaGerak(Gerak arr[], int *n) {
 
         (*n)++; // jika pasangan x dan y berhasil dibaca, count naik
     }
+
+    fclose(fgerak); // file ditutup
 }
 
 // menyimpan semua data koordinat ke file
 void tulisSemuaGerak(Gerak arr[], int n) {
     FILE *fgerak = fopen(FILE_GERAK, "w"); // membuka file tgerak.txt
     if (fgerak == NULL) {   // jika file kosong
-        printf("Error: tidak bisa membuka %s\n", NAMA_FILE_GERAK);
+        printf("Error: tidak bisa membuka %s\n", FILE_GERAK);
         return; // kembali
     }
     
     // mencetak semua data koordinat dari memori ke file
     for (int i = 0; i < n; i++) {
-        fprintf(f, "%d %d\n", arr[i].x, arr[i].y); 
+        fprintf(fgerak, "%d %d\n", arr[i].x, arr[i].y); 
     }
     
-    fclose(fgerak);
+    fclose(fgerak); // file ditutup
 }
 
 // menampilkan data gerak O dalam bentuk tabel
@@ -71,9 +73,32 @@ void tampilTabelGerak(Gerak arr[], int n) {
     printf("----------\n");
     if (n == 0) {   // jika  belum ada data gerak
         printf("  (belum ada data gerak)\n");
-    } else {        // menampilkan data gerak secara perulangan
-        for (int i = 0; i < n; i++)
+    } else {        
+        for (int i = 0; i < n; i++) // menampilkan data gerak dengan perulangan
             printf("%-5d %-5d\n", arr[i].x, arr[i].y);
     }
     printf("----------\n");
+}
+
+void inputGerak(int x, int y) {
+    Gerak temp_arr[MAX_GERAK];
+    int jumlah_data = 0;
+
+    // ambil data dari file terlebih dahulu
+    bacaGerak(temp_arr, &jumlah_data);
+
+    // memastikan array sudah berisi atau belum
+    if (jumlah_data >= MAX_GERAK) {
+        printf("Gagal menambahkan gerak baru, memori sudah penuh!\n");
+        return;
+    }
+
+    // memasukkan koordinat baru ke indeks paling akhir
+    temp_arr[jumlah_data].x = x;
+    temp_arr[jumlah_data].y = y;
+    jumlah_data++;
+
+    // simpan semua data yang sudah di-update ke dalam file
+    tulisSemuaGerak(temp_arr, jumlah_data);
+    printf("Gerak (%d, %d) berhasil ditambahkan.\n", x, y);
 }
